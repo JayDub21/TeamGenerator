84230fdb73f220d5f5ead62982c3253493fac119
JayDub21 <justin.wofford.21@gmail.com>
2020-01-22 18:52:44 -0600

    finished prompt q's

--- app.js
+++ app.js
@@ -4,72 +4,135 @@ const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
 
-let mgr;
+let team = [];
 let teamName;
 
-function mgrTeamGen() {
-    inquirer.prompt([
-
-        // Confirm they are a manger
-        {
-            type: "confirm",
-            name: "mgrConfirm",
-            message: "Are you the team Mangager?"
-        },
-
-        {
-            type: "input",
-            name: "teamName",
-            message: "What is your Team's name?",
-            when: (data) => data.mgrConfirm
-        },
-
-        // Ask user for name
-        {
-            type: "input",
-            name: "mgrName",
-            message: "What is your name?",
-            when: (data) => data.mgrConfirm
-        },
-
-        // Ask for user email
-        {
-            type: "input",
-            name: "mgrEmail",
-            message: "What is your email address?",
-            when: (data) => data.mgrConfirm
-        },
-
-        // Ask for user ID#
-        {
-            type: "input",
-            name: "mgrId",
-            message: "What is your ID number?",
-            when: (data) => data.mgrConfirm
-        },
-
-        // Ask user for office #
-        {
-            type: "input",
-            name: "officeNum",
-            message: "What is your office number?",
-            when: (data) => data.mgrConfirm
-        }
-
-
-
-    ]).then((mgrData) => {
+// Initiate manager Questions
+function mgrGen() {
+    inquirer.prompt(mgrQ).then((mgrData) => {
         console.log(mgrData);
 
         // Assign these answers to Manager 
         mgr = new Manager(mgrData);
+
+        // Asign team name to its own Variable
         let teamName = mgrData.teamName;
         // console.log(teamName);
 
+        // After Manager is created, run team questions.
+        teamGen();
+
     })
-}; mgrTeamGen();
+};
 
 
+function teamGen() {
+    inquirer.prompt(teamQ).then((teamData) => {
+        console.log(teamData);
+
+        const role = teamData.role;
+
+    })
+
+};
+
+mgrGen();
+
+
+
+mgrQ = [
+    // Confirm they are a manger before moving on.
+    {
+        type: "confirm",
+        name: "mgrConfirm",
+        message: "Are you the team Mangager?"
+    },
+
+    {
+        type: "input",
+        name: "teamName",
+        message: "What is your Team's name?",
+        when: (data) => data.mgrConfirm
+    },
+
+    {
+        type: "input",
+        name: "mgrName",
+        message: "What is your name?",
+        when: (data) => data.mgrConfirm
+    },
+
+    {
+        type: "input",
+        name: "mgrEmail",
+        message: "What is your email address?",
+        when: (data) => data.mgrConfirm
+    },
+
+    {
+        type: "input",
+        name: "mgrId",
+        message: "What is your ID number?",
+        when: (data) => data.mgrConfirm
+    },
+
+    {
+        type: "input",
+        name: "officeNum",
+        message: "What is your office number?",
+        when: (data) => data.mgrConfirm
+    }
+];
+
+
+teamQ = [
+
+
+    {
+        type: "list",
+        name: "role",
+        message: "What is the new team member's role?",
+        choices: ["Intern", "Engineer"]
+    },
+
+    {
+        type: "input",
+        name: "teamName",
+        message: "What is the new team member's name?",
+    },
+
+    {
+        type: "input",
+        name: "teamId",
+        message: "What is" + teamData.teamName + "'s ID# ?"
+    },
+
+    {
+        type: "input",
+        name: "teamEmail",
+        message: "What is" + teamData.teamName + "'s email?"
+    },
+
+    {
+        type: "input",
+        name: "github",
+        message: "What is" + teamData.teamName + "'s github username?",
+        when: (teamData) => teamData.role === "Engineer"
+    },
+
+    {
+        type: "input",
+        name: "school",
+        message: "What is" + teamData.teamName + "'s School Name?",
+        when: (teamData) => teamData.role === "Intern"
+    },
+
+    {
+        type: "confirm",
+        name: "addTeammate",
+        message: "Do you want to add another member to the team?"
+    },
+];
 
 
 // Ask for GH username
