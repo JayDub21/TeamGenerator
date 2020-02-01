39afb5cc7592b0cf13b3d91e827ba006e54e6354
JayDub21 <justin.wofford.21@gmail.com>
2020-01-31 18:29:53 -0600

    fixed app.js

--- app.js
+++ app.js
@@ -1,113 +1,80 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+//↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+//              NPM Packages
+//=========================================
+//       Importing lib js files  
+//↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
 
-let team = [];
-let teamName;
 
-//↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
-//              NPM Packages
+
 //=========================================
 //  Question bank for Manager Generation  
 //↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-
 mgrQ = [
-    // Confirm if they are a manger before starting.
-    {
-        type: "confirm",
-        name: "mgrConfirm",
-        message: "Are you the team Mangager?"
-    },
 
-    {
-        type: "input",
-        name: "teamName",
-        message: "What is your Team's name?",
-        when: (data) => data.mgrConfirm
-    },
+    // {
+    //     type: "input",
+    //     name: "teamName",
+    //     message: "What is your Team's name?",
+    //     // when: (data) => data.mgrConfirm
+    // },
 
     {
         type: "input",
         name: "mgrName",
         message: "What is your name?",
-        when: (data) => data.mgrConfirm
+        // when: (data) => data.mgrConfirm
     },
 
     {
         type: "input",
         name: "mgrEmail",
         message: "What is your email address?",
-        when: (data) => data.mgrConfirm
+        // when: (data) => data.mgrConfirm
     },
 
     {
         type: "input",
         name: "mgrId",
         message: "What is your ID number?",
-        when: (data) => data.mgrConfirm
+        // when: (data) => data.mgrConfirm
     },
 
     {
         type: "input",
         name: "officeNum",
         message: "What is your office number?",
-        when: (data) => data.mgrConfirm
+        // ç
+    },
+
+    {
+        type: "number",
+        name: "engineers",
+        message: "How many Engineers are on the team?"
+    },
+
+    {
+        type: "number",
+        name: "interns",
+        message: "How many Interns are on the team?"
     }
 ];
 
 
 //===============================================
-// Question bank for New Team Member Generation
+// Question bank for New Intern Member Generation
 //↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-teamQ = [
-    {
-        type: "list",
-        name: "role",
-        message: "What is the new team member's role?",
-        choices: ["Intern", "Engineer"]
-    },
+intQ = [
 
-    {
-        type: "input",
-        name: "teamName",
-        message: "What is the new team member's name?",
-    },
-
-    {
-        type: "input",
-        name: "teamId",
-        message: "What is new team member's ID# ?"
-    },
-
-    {
-        type: "input",
-        name: "teamEmail",
-        message: "What is new team member's email?"
-    },
-
-    {
-        type: "input",
-        name: "github",
-        message: "What is new team member's github username?",
-        when: (teamData) => teamData.role === "Engineer"
-    },
-
-    {
-        type: "input",
-        name: "school",
-        message: "What is new team member's School Name?",
-        when: (teamData) => teamData.role === "Intern"
-    },
-
-    {
-        type: "confirm",
-        name: "addTeammate",
-        message: "Do you want to add another member to the team?"
-    },
 ];
+
+
 
 
 
@@ -115,120 +82,239 @@ teamQ = [
 //     Inititate Manager Questions
 //↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 
-function mgrGen() {
-    inquirer.prompt(mgrQ).then((mgrData) => {
-        console.log(mgrData);
+async function teamGen() {
+    inquirer.prompt(mgrQ)
+        .then(async function (mgrData) {
 
-        const role = mgrData.mgrConfirm;
-        const mgrName = mgrData.mgrName;
-        const mgrId = mgrData.mgrId;
-        const mgrEmail = mgrData.mgrEmail;
-        const officeNum = mgrData.officeNum;
-
-        if (mgrData.mgrConfirm === true) {
-            mgrData.mgrConfirm === "Manager";
-        }
-
-        // Assign these answers to Manager 
-        mgr = new Manager(mgrName, mgrId, mgrEmail, officeNum);
-        team.push(mgr);
-
-        // Fill teamName var
-        teamName = mgrData.teamName;
-
-        // After Manager is created, run team questions.
-        teamGen(mgr);
-
-    })
-
-};
+            let { mgrName, mgrId, mgrEmail, officeNum, engineers, interns } = mgrData;
 
 
-//=========================================
-//       Inititate Team Questions
-//↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+            let genMgr = new Manager(mgrName, mgrId, mgrEmail, officeNum)
+            console.log(genMgr);
 
-function teamGen(mgr) {
-    inquirer.prompt(teamQ).then((teamData) => {
-        console.log(teamData);
+            let eng = [];
+            for (let i = 0; i < engineers; i++) {
+                await inquirer.prompt([
 
-        const role = teamData.role;
-        const teamName = teamData.teamName;
-        const teamId = teamData.teamId;
-        const teamEmail = teamData.teamEmail;
-        const github = teamData.github;
-        const school = teamData.school;
-        const addTeammate = teamData.addTeammate;
+                    {
+                        type: "input",
+                        name: "engName",
+                        message: "What is the new Engineer's name?"
+                    },
 
-        if (role === "Intern") {
-            const intern = new Intern(teamName, teamId, teamEmail, school);
-            team.push(intern);
-        }
-        else if (role === "Engineer") {
-            const engineer = new Engineer(teamName, teamId, teamEmail, github);
-            team.push(engineer);
-        };
+                    {
+                        type: "input",
+                        name: "engId",
+                        message: "What is new Engineer's ID# ?"
+                    },
 
+                    {
+                        type: "input",
+                        name: "engEmail",
+                        message: "What is new Engineer's email?"
+                    },
 
-        if (addTeammate === true) {
-            teamGen();
-        }
-        else {
-            console.log(mgr);
-            let mgrCard = fs.readFileSync('./templates/manager.html', 'utf8');
-            mgrCard = mgrCard.replace('{{name}}', mgr.getName());
-            mgrCard = mgrCard.replace('{{role}}', mgr.getRole());
-            mgrCard = mgrCard.replace('{{id}}', mgr.getId());
-            mgrCard = mgrCard.replace('{{email}}', mgr.getEmail());
-            mgrCard = mgrCard.replace('{{officeNumber}}', mgr.getOfficeNumber());
-            mgrCard = mgrCard.replace('{{role}}', mgr.getRole());
+                    {
+                        type: "input",
+                        name: "github",
+                        message: "What is new Engineer's github username?"
+                    }
 
-            fs.writeFileSync("./team.html", teamGen(mgr));
+                ]).then((engData) => {
 
-            //Loop thru and grap other team members
-            for (i = 0; i < team.length; i++) {
-                let teamMember = team[i];
-                // cards += teamCard(teamMember);
-                console.log("before card")
-                fs.writeFileSync("./team.html", teamCard(teamMember))
+                    let { engName, engId, engEmail, github } = engData;
+                    let genEng = new Engineer(engName, engId, engEmail, github)
+                    console.log(genEng);
+
+                    eng.push(genEng);
+                })
             }
 
-            // main = main.replace('{{cards}}', cards);
+            let int = [];
+            for (let j = 0; j < interns; j++) {
+                await inquirer.prompt([
 
-            // fs.writeFileSync('./team.html')
+                    {
+                        type: "input",
+                        name: "intName",
+                        message: "What is the new team member's name?"
+                    },
 
-        };
+                    {
+                        type: "input",
+                        name: "intId",
+                        message: "What is new team member's ID# ?"
+                    },
+
+                    {
+                        type: "input",
+                        name: "intEmail",
+                        message: "What is new team member's email?"
+                    },
+
+                    {
+                        type: "input",
+                        name: "school",
+                        message: "What school is the Intern from?"
+                    }
+
+                ]).then((intData) => {
+
+                    let { intName, intId, intEmail, school } = intData;
+
+                    let genInt = new Intern(intName, intId, intEmail, school)
+                    console.log(genInt);
+
+                    int.push(genInt);
 
 
-    });
+                })
 
-};
+            }
+            let { Name, Id, Email, OfficeNum, Role } = genMgr
+
+            let mgrCard = fs.readFileSync('./templates/manager.html', 'utf8');
+            mgrCard = mgrCard.replace('{{name}}', Name);
+            mgrCard = mgrCard.replace('{{role}}', Role);
+            mgrCard = mgrCard.replace('{{id}}', Id);
+            mgrCard = mgrCard.replace('{{email}}', Email);
+            mgrCard = mgrCard.replace('{{officeNumber}}', OfficeNum);
+            console.log(mgrCard);
+
+            let engHTML = [];
+            for (let i = 0; i < eng.length; i++) {
+
+                let { engName, engId, engEmail, github, role } = eng[i];
+
+                let engCard = fs.readFileSync('./templates/engineer.html', 'utf8');
+
+                engCard = engCard.replace('{{name}}', engName);
+                engCard = engCard.replace('{{id}}', engId);
+                engCard = engCard.replace('{{email}}', engEmail);
+                engCard = engCard.replace('{{github}}', github);
+                engCard = engCard.replace('{{role}}', role);
+                engHTML.push(engCard);
+
+            }
+
+            let intHTML = [];
+            for (let j = 0; j < int.length; j++) {
+
+                let { intName, intId, intEmail, school, role } = int[j];
+
+                let intCard = fs.readFileSync('./templates/intern.html', 'utf8');
+
+                intCard = intCard.replace('{{name}}', intName);
+                intCard = intCard.replace('{{id}}', intId);
+                intCard = intCard.replace('{{email}}', intEmail);
+                intCard = intCard.replace('{{school}}', school);
+                intCard = intCard.replace('{{role}}', role);
+                intHTML.push(intCard);
+
+            }
+
+            let mainHtml = fs.readFileSync('./templates/main.html', 'utf8');
+            mainHtml = mainHtml.replace('{{manager}}', mgrCard);
+            mainHtml = mainHtml.replace('{{engineers}}', engHTML);
+            mainHtml = mainHtml.replace('{{interns}}', intHTML);
+
+            fs.writeFileSync('.outputs.html', mainHtml);
+        })
+
+}
+
+teamGen()
+//     function teamCard(member) {
 
 
-// Starts manager questions
-mgrGen();
+
+//         if (Employee.getRole() === "Engineer") {
+//             let engCard = fs.readFileSync('./templates/engineer.html', 'utf8');
+//             engCard = engCard.replace('{{name}}', member.getName());
+//             engCard = engCard.replace('{{role}}', member.getRole());
+//             engCard = engCard.replace('{{id}}', member.getId());
+//             engCard = engCard.replace('{{email}}', member.getEmail());
+//             engCard = engCard.replace('{{github}}', member.getGithub());
+//             return engCard
+//         }
+
+//         else if (Employee.getRole() === "Intern") {
+//             var intCard = fs.readFileSync('./templates/intern.html', 'utf8');
+//             intCard = intCard.replace('{{name}}', member.getName());
+//             intCard = intCard.replace('{{role}}', member.getRole());
+//             intCard = intCard.replace('{{id}}', member.getId());
+//             intCard = intCard.replace('{{email}}', member.getEmail());
+//             intCard = intCard.replace('{{school}}', member.getSchool());
+//             return intCard;
+//         }
+//     };
+
+// })
+
+// };
+
+// teamGen();
+
+// //=========================================
+// //       Inititate Team Questions
+// //↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+
+// function teamGen(mgr) {
+//     console.log(mgr);
+//     inquirer.prompt(teamQ).then((teamData) => {
+//         console.log(teamData);
+
+//         const role = teamData.role;
+//         const firstName = teamData.firstName;
+//         const teamId = teamData.teamId;
+//         const teamEmail = teamData.teamEmail;
+//         const github = teamData.github;
+//         const school = teamData.school;
+//         const addTeammate = teamData.addTeammate;
+
+//         if (role === "Intern") {
+//             const intern = new Intern(firstName, teamId, teamEmail, school);
+//             team.push(intern);
+//         }
+//         else if (role === "Engineer") {
+//             const engineer = new Engineer(firstName, teamId, teamEmail, github);
+//             team.push(engineer);
+//         };
+
+
+//         if (addTeammate === true) {
+//             teamGen();
+//         }
+//         else {
+//             managerCard(mgr);
+//             console.log(mgr);
 
 
 
-function teamCard(member) {
 
-    if (member.getRole() === "Engineer") {
-        let engCard = fs.readFileSync('./templates/engineer.html', 'utf8');
-        engCard = engCard.replace('{{name}}', member.getName());
-        engCard = engCard.replace('{{role}}', member.getRole());
-        engCard = engCard.replace('{{id}}', member.getId());
-        engCard = engCard.replace('{{email}}', member.getEmail());
-        engCard = engCard.replace('{{github}}', member.getGithub());
-        return engCard
-    }
+//             //Loop thru and grab team members
+//             for (i = 0; i < team.length; i++) {
+//                 member = team[i];
+//                 cards += teamCard(member);
+//                 fs.writeFileSync("./team.html", managerCard(mgr))
+//                 fs.writeFileSync("./team.html", teamCard(member));
 
-    else if (member.getRole() === "Intern") {
-        var intCard = fs.readFileSync('./templates/intern.html', 'utf8');
-        intCard = intCard.replace('{{name}}', member.getName());
-        intCard = intCard.replace('{{role}}', member.getRole());
-        intCard = intCard.replace('{{id}}', member.getId());
-        intCard = intCard.replace('{{email}}', member.getEmail());
-        intCard = intCard.replace('{{school}}', member.getSchool());
-        return intCard;
-    }
-};
+
+
+
+//             }
+
+//             let main = fs.readFileSync('./templates/main.html', 'utf8');
+//             main = main.replace("{{teamName}}", teamName);
+//             main = main.replace("{{cards}}", teamCard);
+
+//         };
+
+
+//     });
+
+// };
+
+
+
+
